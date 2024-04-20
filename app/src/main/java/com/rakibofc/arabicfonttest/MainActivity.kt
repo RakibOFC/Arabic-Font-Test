280c5253f61a789069bf9a8cca59943d2bc50d03
Rakibul Islam <rakibull2000@gmail.com>
2024-04-20 18:51:27 +0600

    Initial commit

---
+++ app/src/main/java/com/rakibofc/arabicfonttest/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.rakibofc.arabicfonttest
+
+import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URLEncoder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val specialChar = "اَلِیْمٌ ۙ۬۟ \uF64A— ,أَلِيمُۢ"
+        val urlEncoder = URLEncoder.encode(specialChar, "UTF-8");
+        val word = URLEncoder.encode("\uF64A", "UTF-8")
+
+        val spannedText: Spanned = Html.fromHtml(specialChar, Html.FROM_HTML_MODE_LEGACY)
+
+
+        findViewById<TextView>(R.id.tvTemp).text = specialChar
+
+        /*val word = "هَنِیْٓـًٔا\uF64A"
+        val char2 = "\uF64B"
+
+        for (char in word) {
+            Log.e("TAG", "ASCII code for '$char' is ${char.code}")
+        }*/
+    }
+}
